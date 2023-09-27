634ed597f8ae983f85d1bc8cba2b7def42a32ccf
rainarodrigues <rainarrodrigues@gmail.com>
2023-09-27 08:49:13 -0300

    Adicionando os padroes view code

--- Ricky and Morty UIKit/AppDelegate.swift
+++ Ricky and Morty UIKit/AppDelegate.swift
@@ -12,9 +12,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        // Override point for customization after application launch.
+//        return true
+//    }
+    
+    var window: UIWindow?
+    func application(
+        _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [
+            UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+                window = UIWindow()
+                window?.rootViewController = ViewController ()
+                window?.makeKeyAndVisible()
+            return true
     }
 
     // MARK: UISceneSession Lifecycle
