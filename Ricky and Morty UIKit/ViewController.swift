bde3e4ef9e39483888b073bb9466273e90bc1988
rainarodrigues <rainarrodrigues@gmail.com>
2023-09-27 08:10:41 -0300

    Initial Commit

---
+++ Ricky and Morty UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ricky and Morty UIKit
+//
+//  Created by Raina Rodrigues de Lima on 27/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
